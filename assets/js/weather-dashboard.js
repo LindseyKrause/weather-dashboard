019b41ec65ec9940a925c6fd4d8bd30aa4802255
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-19 22:31:15 -0700

    added weather api

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -1,1 +1,17 @@
 
+// deccf4a9d1ad781cea56007a1e378a5a
+var searchButton = document.querySelector("#searchButton");
+var userInput = document.getElementById("userSearch").value;
+$(searchButton).click(function() {
+    console.log("search button clicked"); 
+    console.log(userInput);
+})
+fetch(
+	'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=3f8b7d7282213e928cf632d579aeff2d'
+)
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log(data);
+	});
