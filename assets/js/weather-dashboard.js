d2e1fd16d6ec51add94db8cdfdbb11d7fa055330
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-23 11:32:29 -0700

    Target user input

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -1,10 +1,10 @@
 
 // deccf4a9d1ad781cea56007a1e378a5a
 var searchButton = document.querySelector("#searchButton");
-var userInput = document.getElementById("userSearch").value;
+var userInput = document.getElementById("userSearch");
 $(searchButton).click(function() {
     console.log("search button clicked"); 
-    console.log(userInput);
+    console.log(userInput.value);
 })
 fetch(
 	'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=3f8b7d7282213e928cf632d579aeff2d'
@@ -15,3 +15,4 @@ fetch(
 	.then(function (data) {
 		console.log(data);
 	});
+ 
