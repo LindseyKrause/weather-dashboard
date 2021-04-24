9f3414953f6ca87ebb8f9b7409112c0aafbfa542
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-23 17:57:51 -0700

    initial api call functionality

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -1,18 +1,35 @@
 
 // deccf4a9d1ad781cea56007a1e378a5a
-var searchButton = document.querySelector("#searchButton");
-var userInput = document.getElementById("userSearch");
-$(searchButton).click(function() {
-    console.log("search button clicked"); 
-    console.log(userInput.value);
+cityDisplay = document.getElementById("todayRow");
+
+$("#searchButton").click(function () {
+	var userInput = document.getElementById("userSearch");
+	console.log("search button clicked");
+	console.log(userInput.value);
+	searchWeather(userInput.value);
 })
-fetch(
-	'http://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=3f8b7d7282213e928cf632d579aeff2d'
-)
-	.then(function (response) {
-		return response.json();
-	})
-	.then(function (data) {
-		console.log(data);
-	});
- 
+function searchWeather (cityName){
+	fetch(
+		`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=3f8b7d7282213e928cf632d579aeff2d&units=imperial`
+	)
+		.then(response => {
+			return response.json();
+		}).then(data => {
+			console.log(data);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+	
+}
+
+
+
+
+	// 	response.text().then(function(text) {
+	// 		cityDisplay.textContent = text;
+	// 	})
+
+	// .then(function (data) {
+	// 	console.log(data);
+	// })
