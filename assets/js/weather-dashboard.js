cd6704ed99f06432754e86d9a0c2b9bf121411c7
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-24 21:08:14 -0700

    added info to 5 day cards

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -1,7 +1,7 @@
 
 // deccf4a9d1ad781cea56007a1e378a5a
 cityDisplay = document.getElementById("todayRow");
-var currentDate = new Date(dateString);
+// var currentDate = new Date(dateString);
 $("#searchButton").click(function () {
 	var userInput = document.getElementById("userSearch");
 	console.log("search button clicked");
@@ -27,23 +27,20 @@ function searchWeather(cityName) {
 function drawWeather(d) {
 	let returnColumn = document.getElementById('returnColumn');
 	let cityHeading = document.getElementById('cityHeading');
+	cityHeading.textContent = d.name;
 	let temp = document.getElementById('temp');
 	temp.textContent = "Temp: " + d.main.temp;
 	let humidity = document.getElementById('humidity');
-	let wind = document.getElementById('windspeed');
+	humidity.textContent = "Humidity: " + d.main.humidity;
+	let wind = document.getElementById('windSpeed');
+	wind.textContent = "Wind: " + d.wind.speed;
 	let uv = document.getElementById('uv');
-	let tempInfo = d.main.temp
-	let p = document.createElement('p');
 	let imgCont = document.querySelector("#todayImgCol .figure");
 	imgCont.innerHTML = "";
 	let img = document.createElement('img');
 	img.setAttribute('src', `http://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`);
 	imgCont.appendChild(img);
 }
-// returnColumn.innerHTML = d.weather[0].description + "Temp" + d.main.temp + "Feels Like" + d.main.feels_like + "Temp Min" + d.main.temp_min + "Temp Max" + d.main.temp_max;
-// document.getElementById('returnColumn').innerHTML = d.name;
-// document.getElementById('returnColumn').innerHTML = d.main.temp;
-// document.getElementById('returnColumn').innerHTML = d.main.feels_like;
 function searchWeather5day(cityName) {
 	fetch(
 		`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=deccf4a9d1ad781cea56007a1e378a5a&cnt=5&units=imperial`
@@ -60,12 +57,54 @@ function searchWeather5day(cityName) {
 		});
 
 	function draw5Weather(d) {
+		let cardHead1 = document.getElementById('cardTitleId1');
+		cardHead1.textContent = d.city.name;
+		let p1 = document.getElementById('cardTextId1');
+		p1.textContent = "Temp: " + d.list[0].main.temp + "   Humidity: " + d.list[0].main.humidity + "   Wind: " + d.list[0].wind.speed;
+
+		let cardHead2 = document.getElementById('cardTitleId2');
+		cardHead2.textContent = d.city.name;
+		let p2 = document.getElementById('cardTextId2');
+		p2.textContent = "Temp: " + d.list[1].main.temp + "   Humidity: " + d.list[1].main.humidity + "   Wind: " + d.list[1].wind.speed;
+
+		let cardHead3 = document.getElementById('cardTitleId3');
+		cardHead3.textContent = d.city.name;
+		let p3 = document.getElementById('cardTextId3');
+		p3.textContent = "Temp: " + d.list[2].main.temp + "   Humidity: " + d.list[2].main.humidity + "   Wind: " + d.list[2].wind.speed;
+
+		let cardHead4 = document.getElementById('cardTitleId4');
+		cardHead4.textContent = d.city.name;
+		let p4 = document.getElementById('cardTextId4');
+		p4.textContent = "Temp: " + d.list[3].main.temp + "   Humidity: " + d.list[3].main.humidity + "   Wind: " + d.list[3].wind.speed;
+
+		let cardHead5 = document.getElementById('cardTitleId5');
+		cardHead5.textContent = d.city.name;
+		let p5 = document.getElementById('cardTextId5');
+		p5.textContent = "Temp: " + d.list[4].main.temp + "   Humidity: " + d.list[4].main.humidity + "   Wind: " + d.list[4].wind.speed;
 
 	}
+
 
 }
 
 
+// let cardDiv = document.getElementById("dayOne");
+// let createCard = document.createElement('div');
+// createCard.className = 'card';
+// let createCardBody = document.createElement('div');
+// createCardBody.className = 'card-body';
+// let createCardTitle = document.createElement('h5');
+// createCardTitle.className = 'card-title';
+// createCardTitle.innerText = d.city.name;
+// let createCardP = document.createElement('p');
+// createCardP.className = 'card-text';
+// let createCardBtn = document.createElement('button');
+// createCardBtn.className = 'btn btn-primary';
+
+// cardDiv.appendChild(createCard, createCardBody);
+// createCardBody.appendChild(createCardTitle, );
+// createCardTitle.textContent = "this is something";
+// createCardP.textContent = 'this is example text'
 
 
 
