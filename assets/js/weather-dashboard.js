90f8b282daf228f184c4a35017b2188e50e57334
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-04-24 17:22:02 -0700

    append weather to page

--- assets/js/weather-dashboard.js
+++ assets/js/weather-dashboard.js
@@ -1,12 +1,13 @@
 
 // deccf4a9d1ad781cea56007a1e378a5a
 cityDisplay = document.getElementById("todayRow");
-
+var currentDate = new Date(dateString);
 $("#searchButton").click(function () {
 	var userInput = document.getElementById("userSearch");
 	console.log("search button clicked");
 	console.log(userInput.value);
 	searchWeather(userInput.value);
+	searchWeather5day(userInput.value);
 })
 function searchWeather(cityName) {
 	fetch(
@@ -24,14 +25,47 @@ function searchWeather(cityName) {
 
 }
 function drawWeather(d) {
-let returnColumn = document.getElementById('returnColumn');
-	returnColumn.innerHTML = d.weather[0].description + "Temp" + d.main.temp + "Feels Like" + d.main.feels_like + "Temp Min" + d.main.temp_min + "Temp Max" + d.main.temp_max;
-	// document.getElementById('returnColumn').innerHTML = d.name;
-	// document.getElementById('returnColumn').innerHTML = d.main.temp;
-	// document.getElementById('returnColumn').innerHTML = d.main.feels_like;
+	let returnColumn = document.getElementById('returnColumn');
+	let cityHeading = document.getElementById('cityHeading');
+	let temp = document.getElementById('temp');
+	temp.textContent = "Temp: " + d.main.temp;
+	let humidity = document.getElementById('humidity');
+	let wind = document.getElementById('windspeed');
+	let uv = document.getElementById('uv');
+	let tempInfo = d.main.temp
+	let p = document.createElement('p');
+	let imgCont = document.querySelector("#todayImgCol .figure");
+	imgCont.innerHTML = "";
+	let img = document.createElement('img');
+	img.setAttribute('src', `http://openweathermap.org/img/wn/${d.weather[0].icon}@2x.png`);
+	imgCont.appendChild(img);
+}
+// returnColumn.innerHTML = d.weather[0].description + "Temp" + d.main.temp + "Feels Like" + d.main.feels_like + "Temp Min" + d.main.temp_min + "Temp Max" + d.main.temp_max;
+// document.getElementById('returnColumn').innerHTML = d.name;
+// document.getElementById('returnColumn').innerHTML = d.main.temp;
+// document.getElementById('returnColumn').innerHTML = d.main.feels_like;
+function searchWeather5day(cityName) {
+	fetch(
+		`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=deccf4a9d1ad781cea56007a1e378a5a&cnt=5&units=imperial`
+	)
+		.then(response => {
+			return response.json();
+		}).then(data => {
+			console.log("five day");
+			console.log(data);
+			draw5Weather(data);
+		})
+		.catch(err => {
+			console.error(err);
+		});
 
+	function draw5Weather(d) {
+
+	}
 
 }
+
+
 
 
 
